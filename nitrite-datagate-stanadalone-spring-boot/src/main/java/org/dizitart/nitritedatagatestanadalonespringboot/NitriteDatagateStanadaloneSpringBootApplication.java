a2fbb1c2906b21fcf5cc766ae2800367a987032a
Tareq Kirresh <kirreshtareq@gmail.com>
2019-12-24 18:45:20 +0200

    Added Spring Boot Projects

---
+++ nitrite-datagate-stanadalone-spring-boot/src/main/java/org/dizitart/nitritedatagatestanadalonespringboot/NitriteDatagateStanadaloneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.dizitart.nitritedatagatestanadalonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitriteDatagateStanadaloneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitriteDatagateStanadaloneSpringBootApplication.class, args);
+	}
+
+}
---
+++ nitrite-datagate-stanadalone-spring-boot/src/test/java/org/dizitart/nitritedatagatestanadalonespringboot/NitriteDatagateStanadaloneSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dizitart.nitritedatagatestanadalonespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitriteDatagateStanadaloneSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
