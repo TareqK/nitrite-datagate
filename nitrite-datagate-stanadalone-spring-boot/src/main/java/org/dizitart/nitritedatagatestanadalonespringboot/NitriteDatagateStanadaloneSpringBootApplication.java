3427afed3c763499bb5a0d6a1ab9e62e5c51cbbd
Tareq Kirresh <kirreshtareq@gmail.com>
2019-12-27 22:19:03 +0200

    created seperate javaee project

--- nitrite-datagate-stanadalone-spring-boot/src/main/java/org/dizitart/nitritedatagatestanadalonespringboot/NitriteDatagateStanadaloneSpringBootApplication.java
+++ nitrite-datagate-stanadalone-spring-boot/src/main/java/org/dizitart/nitritedatagatestanadalonespringboot/NitriteDatagateStanadaloneSpringBootApplication.java
@@ -1,13 +1,16 @@
 package org.dizitart.nitritedatagatestanadalonespringboot;
 
+import org.dizitart.nitrite.datagate.spring.boot.configuration.DatagateHandlerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(DatagateHandlerConfiguration.class)
 public class NitriteDatagateStanadaloneSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NitriteDatagateStanadaloneSpringBootApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(NitriteDatagateStanadaloneSpringBootApplication.class, args);
+  }
 
 }
